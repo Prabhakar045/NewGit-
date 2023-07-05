87e58d8bd52ce87ce3b401508153a6e61bafa0e1
prabhakar kattimani <prabhukattimani18@gmail.com>
2023-07-05 09:13:45 +0530

    added message

---
+++ add.java
@@ -0,0 +1,16 @@
+package NewGit;
+
+public class add {
+public static void main(String additionition[])
+{
+addition(56 , 67, 34);
+addition(76 , 56 ,78);     // just to execute we require JRE 
+addition(56 , 98 ,56);       // jdk+JRE+JVM = planform dependent          
+addition(43 , 54 ,54);       //byte code is platform independent
+addition(43 , 54 ,54);       //byte code is platform independent
+
+}
+public static void addition(int z,int u,int v){
+System.out.println(z+u+v);
+}
+}
