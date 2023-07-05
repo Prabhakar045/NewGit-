a5b678e3cce3c348012a141cd124bb002ad95699
prabhakar kattimani <prabhukattimani18@gmail.com>
2023-07-05 09:33:17 +0530

    added implementation for multi.java

---
+++ multi.java
@@ -0,0 +1,12 @@
+package NewGit;
+
+public class multi {
+    public static void main(String[] args) {
+        mul(1,2);
+        mul(3,6);
+    }
+
+    public static void mul(int a, int b){
+        System.out.println(a*b);
+    }
+}
